a61eb0bacb70ad79e090a79744e6a65711cbcea3
croftonweed <53463228+croftonweed@users.noreply.github.com>
2024-05-28 13:28:20 +0800

    Update Twitter.vue

--- components/home/Twitter.vue
+++ components/home/Twitter.vue
@@ -9,7 +9,7 @@ import { ArrowRight } from 'lucide-vue-next'
     target="_blank"
     class="inline-flex items-center px-3 py-1 mx-auto my-4 space-x-1 text-sm font-medium rounded-lg bg-muted"
   >
-    <XIcon class="w-4 h-4" />
+    <F5Icon class="w-4 h-4" />
     <Separator
       orientation="vertical"
       class="h-4"
