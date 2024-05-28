0539841e59a8ab4b1823739ff1ffcbb6cdf7f1bf
croftonweed <53463228+croftonweed@users.noreply.github.com>
2024-05-28 13:23:23 +0800

    Update Twitter.vue

--- components/home/Twitter.vue
+++ components/home/Twitter.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { XIcon } from 'vue3-simple-icons'
+import { F5Icon } from 'vue3-simple-icons'
 import { ArrowRight } from 'lucide-vue-next'
 </script>
 
 <template>
   <a
-    href="https://x.com/holyapp"
+    href="https://www.f5.com"
     target="_blank"
     class="inline-flex items-center px-3 py-1 mx-auto my-4 space-x-1 text-sm font-medium rounded-lg bg-muted"
   >
@@ -14,7 +14,7 @@ import { ArrowRight } from 'lucide-vue-next'
       orientation="vertical"
       class="h-4"
     />
-    <span>Follow me on X(Twitter)</span>
+    <span>Get more information </span>
     <ArrowRight class="w-4 h-4" />
   </a>
 </template>
